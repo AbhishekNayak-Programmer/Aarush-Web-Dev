7c1a6ddf30a7cdb54d658d1a942611458ef90486
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-07-04 07:31:27 +0530

    Session 13 Done

---
+++ Day 13/script.js
@@ -0,0 +1,45 @@
+console.log("Abhishek Nayak");
+console.log((95 * 45) / 78 - (2345 * 457585) / 345 - 475 + 567680);
+
+let x = (95 * 45) / 78 - (2345 * 457585) / 345 - 47 + 567680;
+console.log(x);
+x = 5 * 5;
+console.log(x);
+
+const name = "Abhishek Nayak";
+console.log(name);
+
+var a = 19 % 3;
+console.log(a);
+
+let isPlaying = false;
+console.log(isPlaying);
+
+let homework;
+console.log(homework);
+
+let test = null;
+console.log(test);
+
+let obj = {
+  name: "Abhishek Nayak",
+  age: 26,
+  profession: "Robotics Software Developer",
+  country: "India",
+};
+
+console.log(obj);
+console.log(obj.profession);
+console.log(obj.age);
+console.log(obj.name);
+console.log(obj.country);
+
+let arr = [12, 456, 78, 9, 8, 90];
+console.log(arr);
+
+let fruits = ["Apple", "Orange", "Banana"];
+console.log(fruits);
+
+//Homework
+// 1. Complete the Netflix Screen
+// 2. Practice Todays session by creating objects and arrays and print it in console
