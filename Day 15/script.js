1bcf237ef3581fac0475a3fb4f5b742e53a705c2
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-07-20 07:31:56 +0530

    Session 15 Done

---
+++ Day 15/script.js
@@ -0,0 +1,31 @@
+// Math Methods
+let num = 45.66;
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+
+num = "54674345tgfddfghytr,e";
+console.log(parseInt(num));
+
+num = "1234.654vgtrd";
+console.log(parseFloat(num));
+
+num = 25;
+console.log(Math.sqrt(num));
+
+num = 2;
+console.log(Math.pow(num, 10));
+
+num = 456.567;
+console.log(Math.trunc(num));
+
+num = 71234;
+console.log(num.toString(2));
+
+num = 12;
+console.log(num.toFixed(2));
+
+console.log(Math.min(123, 345, 23, 567, 8, 9876));
+
+console.log(Math.max(123, 345, 23456, 78, 2));
+
+console.log(Math.trunc(Math.random() * 5 + 5 + 1));
